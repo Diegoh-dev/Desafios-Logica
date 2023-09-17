4216b5712f86121fd79a07ca8d44cef8ba9fb5dc
Diegoh-dev <diegoh.developer@gmail.com>
2023-09-17 11:46:00 -0300

    Converter valores.

---
+++ ConverterValores/index.js
@@ -0,0 +1,14 @@
+/*
+DESCRIÇÃO: 
+Implemente uma função que converta o valor booleano fornecido em sua representação de string.
+
+Nota: Somente entradas válidas serão fornecidas.
+*/
+
+function booleanToString(b) {
+  // return b ? 'true' : 'false'
+  return b.toString();
+}
+
+booleanToString(true);
+booleanToString(false);
