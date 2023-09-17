9aa3e830073566de35fc3e85144f96f5d0b40b42
Diegoh-dev <diegoh.developer@gmail.com>
2023-09-17 10:47:46 -0300

    exercicio retornar o comprimento da menor palavra.

--- index.js
+++ index.js
@@ -146,8 +146,8 @@ function smash (words) {
    return newWPhrase.trim();
 };
 
-console.log(smash(["this", "is", "a", "really", "long", "sentence"]));
-console.log(smash(["hello", "world"]));
+// console.log(smash(["this", "is", "a", "really", "long", "sentence"]));
+// console.log(smash(["hello", "world"]));
 
 
 /*
@@ -164,10 +164,34 @@ function booleanToString(b){
 }
 
 
+// booleanToString(true)
+// booleanToString(false)
 
 
+/*
+Simples, dada uma sequência de palavras, retorne o comprimento da(s) palavra(s) mais curta(s).
 
+A string nunca estará vazia e você não precisa levar em conta diferentes tipos de dados.
+ */
 
+function findShort(s) {
+  const arrayPlavras = s.split(" ");
+  let lengthPalavra = arrayPlavras[0].length;
+
+ arrayPlavras.forEach((element) => {
+  if(lengthPalavra > element.length){
+    lengthPalavra = element.length
+  }
+  // lengthPalavra = element[0];
+  // console.log(element[0])
+  });
+
+  return lengthPalavra
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
+console.log(findShort("Booom dia"))
+console.log(findShort("oi bom dia"))
 
 
 
