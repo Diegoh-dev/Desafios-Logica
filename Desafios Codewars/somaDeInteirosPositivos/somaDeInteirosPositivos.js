38375eb4205fc97eb6ddb87e6e362746b42729e6
Diegoh-dev <diegoh.developer@gmail.com>
2023-10-20 00:20:46 -0300

    somae os dos dois números positivos mais baixos do array.

--- Desafios Codewars/somaDeInteirosPositivos/somaDeInteirosPositivos.js
+++ Desafios Codewars/somaDeInteirosPositivos/somaDeInteirosPositivos.js
@@ -10,9 +10,43 @@ Por exemplo, quando uma matriz é passada como [19, 5, 42, 2, 77], a saída deve
 
 */
 
-function sumTwoSmallestNumbers(numbers) {  
- 
-  return teste
+// Funcionando porem ver uma forma melhor de fazer;
+
+function sumTwoSmallestNumbers(numbers) {
+  let arrayMenor = [];
+  let menorNumero = Math.min(...numbers);
+
+  numbers.map((item, index, array) => {
+    menorNumero = Math.min(...array);
+    arrayMenor[0] = menorNumero;
+
+    array
+      .filter((item) => item !== menorNumero)
+      .map((item2, index, array2) => {
+        arrayMenor[1] = Math.min(...array2);
+      });
+  });
+
+  // console.log({arrayMenor})
+  return arrayMenor.reduce((acc, item) => (acc += item), 0);
 }
 
-console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]))
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));//7
+console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]));//3453455
+
+
+// RESOLUÇÃO CHAT GPT
+
+/*
+Nesta versão, os passos são os seguintes:
+
+1. Copiamos o array numbers para evitar modificar o array original.
+2. Classificamos o array copiado em ordem crescente.
+3. Pegamos os dois números menores do array classificado usando slice(0, 2).
+4. Somamos esses dois números e retornamos o resultado.
+*/
+function sumTwoSmallestNumbers(numbers) {
+  const sortedNumbers = [...numbers].sort((a, b) => a - b);
+  const [smallest1, smallest2] = sortedNumbers.slice(0, 2);
+  return smallest1 + smallest2;
+}
